d63a15c8a5c55fe3e760a577a1896c9bed9fd9f4
Meiirlan Suiirkhanov <meyrlansuirkhnaov@gmail.com>
2022-12-03 20:40:16 +0600

    APIs

--- www/api/v2/client/serializers.py
+++ www/api/v2/client/serializers.py
@@ -85,12 +85,123 @@ class PatientCreateSerializer(serializers.ModelSerializer):
         )
 
 
+class DoctorCreateSerializer(serializers.ModelSerializer):
+    registration_date = serializers.DateField(read_only=True)
+    phone = PhoneNumberField(write_only=True)
+    birth_date = serializers.DateField(write_only=True)
+    password = serializers.CharField(write_only=True)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        phone = validated_data.pop("phone")
+        password = validated_data.pop("password")
+        birth_date = validated_data.pop("birth_date")
+
+        user = User.objects.filter(phone=phone).first()
+        if user:
+            raise ValidationError({"error": f"User with phone {phone} already exists."})
+
+        user = User.objects.create_user(
+            phone=phone,
+            password=password,
+            birth_date=birth_date,
+        )
+        doctor = Doctor.objects.create(**validated_data)
+        doctor.user = user
+        doctor.save()
+
+        return doctor
+
+    class Meta:
+        model = Doctor
+        fields = (
+            "id",
+            "iin_number",
+            "id_number",
+            "name",
+            "surname",
+            "middle_name",
+            "department_id",
+            "specialization_details_id",
+            "experience_years",
+            "photo",
+            "category",
+            "price_per_appointment",
+            "schedule_details",
+            "degree",
+            "rating",
+            "address",
+            "homepage_url",
+            "registration_date",
+            "phone",
+            "birth_date",
+            "password",
+        )
+
+
 class ProfileInfoSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField()
+    surname = serializers.SerializerMethodField()
+    middle_name = serializers.SerializerMethodField()
+
+    def get_name(self):
+        if self.doctor:
+            return self.doctor.name
+        elif self.admin:
+            return self.admin.name
+        elif self.patient:
+            return self.patient.name
+        return None
+
+    def get_surname(self):
+        if self.doctor:
+            return self.doctor.surname
+        elif self.admin:
+            return self.admin.surname
+        elif self.patient:
+            return self.patient.surname
+        return None
+
+    def get_middle_name(self):
+        if self.doctor:
+            return self.doctor.middle_name
+        elif self.admin:
+            return self.admin.middle_name
+        elif self.patient:
+            return self.patient.middle_name
+        return None
+
     class Meta:
         model = User
-        fields = ("id", "phone", "birth_date", "role")
+        fields = ("id", "phone", "birth_date", "role", "name", "surname", "middle_name")
 
 
-# class DoctorInfoSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Doctor
+class DoctorInfoSerializer(serializers.ModelSerializer):
+    registration_date = serializers.DateField(read_only=True)
+    phone = PhoneNumberField(source="user.phone")
+    birth_date = serializers.DateField(source="user.birth_date")
+
+    class Meta:
+        model = Doctor
+        fields = (
+            "id",
+            "iin_number",
+            "id_number",
+            "name",
+            "surname",
+            "middle_name",
+            "department_id",
+            "specialization_details_id",
+            "experience_years",
+            "photo",
+            "category",
+            "price_per_appointment",
+            "schedule_details",
+            "degree",
+            "rating",
+            "address",
+            "homepage_url",
+            "registration_date",
+            "phone",
+            "birth_date",
+        )
--- www/api/v2/client/urls.py
+++ www/api/v2/client/urls.py
@@ -2,16 +2,19 @@ from django.urls import path
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from api.v2.client.views import PatientInfoAPIView, PatientsCreateAPIView, ProfileAPIView
+from api.v2.client.views import PatientsListAPIView, PatientsCreateAPIView, ProfileAPIView, DoctorInfoAPIView, \
+    PatientInfoAPIView, DoctorsListAPIView, DoctorsCreateAPIView
 
 router = routers.SimpleRouter(trailing_slash=False)
 
 urlpatterns = [
     path('patients/<int:pk>', PatientInfoAPIView.as_view(), name='patients'),
+    path('patients', PatientsListAPIView.as_view(), name='patients'),
     path('patients/create', PatientsCreateAPIView.as_view(), name='patients'),
-    path('profile', ProfileAPIView.as_view(), name='profile'),  # Information about user -> role, name, surname
-    # path('doctors/<int:pk>', DoctorInfoAPIView.as_view(), name='doctor'),
-    # path('departments', DepartmentsListAPIView.as_view(), name='departments'),
+    path('profile', ProfileAPIView.as_view(), name='profile'),
+    path('doctors/<int:pk>', DoctorInfoAPIView.as_view(), name='doctors'),
+    path('doctors', DoctorsListAPIView.as_view(), name='doctors'),
+    path('doctors/create', DoctorsCreateAPIView.as_view(), name='doctors'),
     # path('appointments', AppointmentsListAPIView.as_view(), name='appointments'),
     # path('appointments/create', AppointmentCreateAPIView.as_view(), name='appointments'),  # Only patients can create
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
