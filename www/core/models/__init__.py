35e0452389af6122aa9cdb1233c9860ad742dd75
Meiirlan Suiirkhanov <meyrlansuirkhnaov@gmail.com>
2022-12-07 21:38:33 +0600

    migrations fixed

--- www/core/models/__init__.py
+++ www/core/models/__init__.py
@@ -2,3 +2,4 @@ from core.models.user import User
 from core.models.patient import Patient
 from core.models.doctor import Doctor
 from core.models.admin import Admin
+from core.models.appointment import Appointment
